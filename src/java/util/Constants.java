7f07ac7b8a0d4e597cab078e8adb7ee147a12921
Huynhnmt <155714065+huynh5079@users.noreply.github.com>
2025-03-03 23:54:31 +0700

    Create Constants.java

---
+++ src/java/util/Constants.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author nghuy
+ */
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "514572896918-n9g3192o5clrd9v4s708ffjp819vlok6.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/QuickRent/LoginGoogle";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+}
+
+
